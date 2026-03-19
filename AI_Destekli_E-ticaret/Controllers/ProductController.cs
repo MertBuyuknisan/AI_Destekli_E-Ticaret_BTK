03273fa5a1ec67a743cec9b7e143a343dff1e857
Mert Buyuknisan <mbuyuknisan1@gmail.com>
2026-03-19 13:21:44 +0300

    detay ve güncelleme işlemleri yapıldı

--- AI_Destekli_E-ticaret/Controllers/ProductController.cs
+++ AI_Destekli_E-ticaret/Controllers/ProductController.cs
@@ -7,6 +7,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using System.IO;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Formats.Tar;
 
 namespace AI_Destekli_E_ticaret.Controllers;
 
@@ -95,5 +97,75 @@ public class ProductController : Controller
         _context.Products.Remove(product);
         await _context.SaveChangesAsync();
         return RedirectToAction("GetProduct", "Product");
+    }
+
+
+    [Authorize(Roles = "Admin")]
+
+    public IActionResult Update(int id)
+    {
+        var product = _context.Products.Find(id);
+        if (product is null)
+        {
+            return NotFound();
+        }
+        return View(product);
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpPost]
+    public async Task<IActionResult> Update(int id, Product product, IFormFile? imageFile)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(product);
+        }
+
+        var mevcutVeri = await _context.Products.FindAsync(id);
+
+        if (mevcutVeri is null)
+        {
+            return NotFound();
+        }
+
+        mevcutVeri.Ad = product.Ad;
+        mevcutVeri.Aciklama = product.Aciklama;
+        mevcutVeri.Fiyat = product.Fiyat;
+        mevcutVeri.Kategori = product.Kategori;
+        mevcutVeri.Stok = product.Stok;
+
+
+        if (imageFile != null && imageFile.Length > 0)
+        {
+            string wwwRootPath = _hostEnvironment.WebRootPath;
+
+            // Güvenli ve benzersiz bir dosya adı oluşturmak için Guid
+            string fileName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
+            string basePath = Path.Combine(wwwRootPath, "images");
+            string finalPath = Path.Combine(basePath, fileName);
+
+            // /images klasörü yoksa oluştur
+            if (!Directory.Exists(basePath))
+            {
+                Directory.CreateDirectory(basePath);
+            }
+
+            // Dosyayı sunucuya kopyala
+            using (var fileStream = new FileStream(finalPath, FileMode.Create))
+            {
+                await imageFile.CopyToAsync(fileStream);
+            }
+
+            // Veritabanına kaydedilecek yolu modele atadıö.
+            mevcutVeri.ResimUrl = "/images/" + fileName;
+        }
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction("GetProduct", "Product");
+
+
+
+
+
     }
 }
